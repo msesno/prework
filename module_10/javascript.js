b6f3792991ebedad8e178f6845a2033e2297e9e5
msesno <msesno@gmail.com>
2019-03-20 14:08:34 -0400

    initial code push

---
+++ module_10/javascript.js
@@ -0,0 +1,30 @@
+document.getElementById("growBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+
+
+document.getElementById("resetBtn").addEventListener("click", function(){
+
+     document.getElementById("box").style.height = "150px";
+	 document.getElementById("box").style.backgroundColor = "orange";
+
+});
+
+
+
+
+document.getElementById("fadeBtn").addEventListener("click", function(){
+
+     document.getElementById("box").style.backgroundColor = "white";
+
+});
+
+
+document.getElementById("blueBtn").addEventListener("click", function(){
+
+     document.getElementById("box").style.backgroundColor = "blue";
+
+});
